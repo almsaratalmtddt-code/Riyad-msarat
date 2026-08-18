108362c041dc023e45edbe9fec6ae0f8773b7eb8
almsaratalmtddt-code <almsaratalmtddt@gmail.com>
2026-08-18 17:27:34 +0300

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,4 @@
+
+timport "./style.css";
+texport const metadata={title: "ERP 19 - المسارات المتعددة", description: "نظام ERP متكامل"};
+texport default function RootLayout({children}) freturn <html lang="ar" dir="rtl"><body>{children}</|
