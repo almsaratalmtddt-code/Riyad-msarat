344e9212040701ad7c489b7c0c3bc9d7cd2bac43
almsaratalmtddt-code <almsaratalmtddt@gmail.com>
2026-08-18 22:08:06 +0300

    Delete app/layout.jsx

--- app/layout.jsx
+++
@@ -1,4 +0,0 @@
-
-timport "./style.css";
-texport const metadata={title: "ERP 19 - المسارات المتعددة", description: "نظام ERP متكامل"};
-texport default function RootLayout({children}) freturn <html lang="ar" dir="rtl"><body>{children}</|
